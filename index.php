026b7004590237733416e4537408b600330a9494
João Felipe <joaofelipecoutof@gmail.com>
2022-06-24 10:23:39 -0300

    Atualização
    Classe usuario criada anteriormente foi preenchida, código funcionando e imprimindo valores em json

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 
 require_once("config.php");
 
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+
+echo "====================================<br>====================================<br>";
+
+*/
+
+$root = new Usuario();
+
+$root->loadById(2);
+
+echo $root;
 
 ?>
