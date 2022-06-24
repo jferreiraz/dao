c512968bea15fcfef5aaaea7dcda57e1a4180176
João Felipe <joaofelipecoutof@gmail.com>
2022-06-24 12:50:38 -0300

    Adição classe usuario e index
    Novas funções na classe usuario, utilização dessas funções na index, comentários na index.

--- class/Usuario.php
+++ class/Usuario.php
@@ -36,12 +36,13 @@ class Usuario {
         $sql = new sql();
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
         if (count($results) > 0) {
-            $row = $results[0];
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            //$row = $results[0];
+            //$this->setIdusuario($row['idusuario']);
+            //$this->setDeslogin($row['deslogin']);
+            //$this->setDessenha($row['dessenha']);
+            //$this->setDtcadastro(new DateTime($row['dtcadastro']));
 
+            $this->setData($results[0]);  // Função substitui essa inserção
         }
     }
 
@@ -59,14 +60,46 @@ class Usuario {
         $sql = new sql();
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login,":PASSWORD"=>$password));
         if (count($results) > 0) {
-            $row = $results[0];
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            //$row = $results[0];
+            //$this->setIdusuario($row['idusuario']);
+            //$this->setDeslogin($row['deslogin']);
+            //$this->setDessenha($row['dessenha']);
+            //$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+            $this->setData($results[0]);  // Função substitui essa inserção
     }else{
         throw new Exception ("Login e/ou senha inválidos.");
     }}
+
+    public function setData($data){
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+
+    public function insert(){
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", 
+            array(':LOGIN'=>$this->getDeslogin(),':PASSWORD'=>$this->getDessenha()));
+
+        if (count($results) > 0){
+            $this->setData($results[0]);
+        }
+    }
+
+    public function update($login,$password){
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+        $sql = new Sql();
+        $sql->execQuery("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", 
+            array(':LOGIN'=>$this->getDeslogin(),':PASSWORD'=>$this->getDessenha(),':ID'=>$this->getIdusuario()));
+    }
+
+    public function __construct($login = "",$password = ""){ // = "" não é necessário, é uma função que deixa os campos como 'NOT NULL'
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+    }
 
     public function __toString(){
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,34 +2,61 @@
 
 require_once("config.php");
 
-/* //Carrega todos os registros
+/* 
+//Carrega todos os registros
 $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-/* //Carrega um usuário especifico de loadbyid
+/* 
+//Carrega um usuário especifico de loadbyid
 $root = new Usuario(); // Cria novo objeto da classe Usuario
 $root->loadById(2); // Com o objeto carrega a função de Usuario (id de número 2)
 echo $root;  // Imprime na tela (a função loadById junto a toostring imprime em json)
 */
 
-/* //Carrega uma lista de usuários
-
+/* 
+//Carrega uma lista de usuários
 $lista = Usuario::getList();
 echo json_encode($lista);
-
 */
 
-/* //Carrega uma lista de usuários buscando pelo login
-
+/* 
+//Carrega uma lista de usuários buscando pelo login
 $search = Usuario::search("FF"); // é necessário ter FF escrito no login para ser encontrado e impresso
 echo json_encode($search);
 
-*/ //Carrega um usuário usando o login e a senha
+*/ 
 
+/* 
+//Carrega um usuário usando o login e a senha
 $usuario = new Usuario();
 $usuario->login("JoaoFF","1231239683"); // O usuário e senha de login deve ser iguais aos que estão registrados no banco de dados
+echo $usuario;
+*/
+
+/* 
+//Inserção de dados na tabela usando stored procedure
+$aluno = new Usuario();                // Antes da criação da função __construct na classe Usuario
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("123543");
+$aluno->insert();
+echo $aluno;
+*/
+
+/*
+//Inserção de dados na tabela usando stored procedure com a função __construct
+$aluno = new Usuario("Rodrigo","431315555");   // Após a criação da função __construct na classe Usuario
+$aluno->insert();
+echo $aluno;
+*/
+
+//Alteração de dados da tabela
+$usuario = new Usuario();
+$usuario->LoadById(15);
+$usuario->update("Ana Luisa","133232662");
+
 echo $usuario;
 
 ?>
